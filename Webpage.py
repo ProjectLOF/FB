6ff57eba4e50867dea3c232aecfd6c6adb78d31b
andamani <viveksubramanian90@gmail.com>
2024-04-21 11:31:24 +0200

    Aded new question for repeat intension

--- Webpage.py
+++ Webpage.py
@@ -23,7 +23,7 @@ def test_db(dish_recommendation,customer_service, ambience_service, user_comment
 
 
 
-def update_db(dish_recommendation,customer_service, ambience_service, user_comment, order,dish_improvement,user_improvement):
+def update_db(dish_recommendation,customer_service, ambience_service, user_comment, order,dish_improvement,user_improvement,repeat_intension):
 
       uri = "mongodb+srv://{}:{}@projectlofcluster.kxjkvqf.mongodb.net/?retryWrites=true&w=majority&appName=ProjectLOFCluster".format(st.secrets["db_username"],st.secrets["db_password"] )
       # Create a new client and connect to the server
@@ -35,7 +35,8 @@ def update_db(dish_recommendation,customer_service, ambience_service, user_comme
          db = client["feedback_v0"]
          collection = db["poc_v1"]
          document = {"table_number": "12", "datetime":str(datetime.now()),"recommendation": dish_recommendation, "cs_rating":customer_service, 
-         "ambience_service": ambience_service, "user_comments": user_comment, "order": order,"dish_to_improvement":dish_improvement,"improvements": user_improvement}
+         "ambience_service": ambience_service, "user_comments": user_comment, 
+         "order": order,"dish_to_improvement":dish_improvement,"improvements": user_improvement, "repeat_intension": repeat_intension}
          inserted_document = collection.insert_one(document)
          client.close()
       except Exception as e:
@@ -62,7 +63,6 @@ def get_latest_orders():
    client = MongoClient(uri, server_api=ServerApi('1'))
    try:
          client.admin.command('ping')
-         st.write("Successfully connect to the Database")
          db = client["order_db_v0"]
          collection = db["demo_setup_v0"]
          db_response =list(collection.find().sort([('_id', -1)]).limit(1))
@@ -73,33 +73,38 @@ def get_latest_orders():
    except Exception as e:
       st.write(e)
     
+def get_latest_questions():
+   uri = "mongodb+srv://{}:{}@projectlofcluster.kxjkvqf.mongodb.net/?retryWrites=true&w=majority&appName=ProjectLOFCluster".format(st.secrets["db_username"],st.secrets["db_password"] )
+   client = MongoClient(uri, server_api=ServerApi('1'))
+   try:
+         client.admin.command('ping')
+         db = client["order_db_v0"]
+         collection = db["demo_questions_v0"]
+         db_response =list(collection.find().sort([('_id', -1)]).limit(1))
+         questions = db_response[0]["questions_list"]
+         return questions
 
-
+   except Exception as e:
+      st.write(e)
 
 st.header("Thank you for visiting us and sharing your :blue[Feedback]!!! :sunglasses:")
 
 orders = get_latest_orders()
-
-with st.container(border=True):
-    
+        
+with st.container(border=True):    
    dish_improvement = st.multiselect("Dish to be Improved?", orders,default="None")
    user_improvement = "NA"
    if len(dish_improvement) > 0 and "None" not in dish_improvement :
             user_improvement = st.selectbox("What are the improvements?", ["Need more Spice","Need more Salt","Need Less Spice","Need less Salt", "Drop the dish"])
-with st.form("Feedback", clear_on_submit=True):
-   with st.container(border=True):  
-      dish_recommendation = st.multiselect("Recommend dish to a friend",orders,default="None",)
-   with st.container(border=True):   
-      customer_service = st_star_rating("Please Rate Our Customer Service", maxValue=5, defaultValue=3, key="cs_rating_form")
-   with st.container(border=True):
-      ambience_service = st_star_rating("Please Rate Our Ambience ", maxValue=5, defaultValue=3, key="ambience_rating_form")
-   with st.container(border=True):
-      user_comment = st.text_input("More Feedback?")
-
-   
+with st.form("Feedback", clear_on_submit=True): 
+   repeat_intension = st.selectbox("Will you visit us again?", ["Yes", "Undecided", "Never"])
+   dish_recommendation = st.multiselect("Recommend dish to a friend",orders,default="None",)
+   customer_service = st_star_rating("Please Rate Our Customer Service", maxValue=5, defaultValue=3, key="cs_rating_form")
+   ambience_service = st_star_rating("Please Rate Our Ambience ", maxValue=5, defaultValue=3, key="ambience_rating_form")
+   user_comment = st.text_input("More Feedback?")
    submit = st.form_submit_button("Feedback_submit")
    if submit:
-      update_db(dish_recommendation,customer_service, ambience_service, user_comment, orders,dish_improvement,user_improvement)
-
+      update_db(dish_recommendation,customer_service, ambience_service, user_comment, 
+                orders,dish_improvement,user_improvement,repeat_intension)
       st.write("Your feedback has been stored to improve your experience in the future")
 
